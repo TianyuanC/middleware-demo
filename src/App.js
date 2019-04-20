a1fcfbe1f7f6078ad7cd6be75d8c89ad8844da53
Ryan Chu <tianyuan.chu@gmail.com>
2019-04-20 11:04:17 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>{this.props.value}</p>
+                    <a
+                        href="#increment"
+                        onClick={this.props.onIncrement}
+                        className="App-link"
+                    >
+                        Increment
+                    </a>
+                    <a
+                        href="#decrement"
+                        onClick={this.props.onDecrement}
+                        className="App-link"
+                    >
+                        Decrement
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(counter);
+
+const render = () =>
+    ReactDOM.render(
+        <App
+            value={store.getState()}
+            onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+            onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+        />,
+        document.getElementById("root")
+    );
+
+render();
+store.subscribe(render);
